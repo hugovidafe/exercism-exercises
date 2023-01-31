de66d823b2b0febb24c19c2a134e8fa03169d180
Hugovidafe <gh@hugovidafe.dev>
2023-01-31 15:18:42 +0000

    🚨 (typescript) fix tests

--- typescript/resistor-color/resistor-color.test.ts
+++ typescript/resistor-color/resistor-color.test.ts
@@ -1,21 +1,21 @@
-import { colorCode, COLORS } from './resistor-color';
+import { colorCode, COLORS } from './resistor-color'
 
 describe('color code', () => {
 	it('Black', () => {
-		expect(colorCode('black')).toEqual(0);
-	});
+		expect(colorCode('black')).toEqual(0)
+	})
 
 	it('White', () => {
-		expect(colorCode('white')).toEqual(9);
-	});
+		expect(colorCode('white')).toEqual(9)
+	})
 
 	it('Orange', () => {
-		expect(colorCode('orange')).toEqual(3);
-	});
-});
+		expect(colorCode('orange')).toEqual(3)
+	})
+})
 
 describe('Colors', () => {
 	it('returns all colors', () => {
-		expect(COLORS).toEqual(['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']);
-	});
-});
+		expect(COLORS).toEqual(['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'])
+	})
+})
